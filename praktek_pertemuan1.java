3217e1abf0ed090eee6ac765748f86a44860ffcf
friskyamirontoneng <80090825+friskyamirontoneng@users.noreply.github.com>
2021-03-09 15:38:56 +0700

    Create praktek_pertemuan1.java

---
+++ praktek_pertemuan1.java
@@ -0,0 +1,24 @@
+public class praktek1 {
+    public static void main(String[] args) {
+        soal1();
+        Soal2();
+        soal3();
+        soal4();
+    }
+    privat static void soal1() {
+        int result = -5 + 8 * 6;
+        system.out.println(result);
+    }
+    privat static void soal2() {
+        int result = (55+9) % 9;
+        system.out.println(result);
+    }
+    privat static void soal3() {
+        int result = 20 + -3*5 / 8;
+        system.out.println(result);
+    }
+    privat static void soal4() {
+        int result = 5 + 15 / 3 * 2 - 8 % 3;
+        system.out.println(result);
+    }
+}
