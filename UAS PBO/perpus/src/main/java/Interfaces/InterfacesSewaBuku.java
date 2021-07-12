d98a036455be6659889c6a5e81f3b35b6001690a
friskyamirontoneng <80090825+friskyamirontoneng@users.noreply.github.com>
2021-07-11 20:19:38 -0700

    Create InterfacesSewaBuku.java

---
+++ UAS PBO/perpus/src/main/java/Interfaces/InterfacesSewaBuku.java
@@ -0,0 +1,17 @@
+/*
+Nama: Friskya Mirontoneng
+Nim: A11.2019.11956
+Kelas: A11.4424
+ */
+package Interfaces;
+
+import Model.mTable; /Untuk memanggil mTable.java dalam package Model/
+import java.util.List; /*Untuk memanggil package List pada java */
+
+/**
+ *
+ * @author Friskya 
+ */
+public interface InterfacesSewaBuku {
+    public List<mTable> GetAll();
+}
