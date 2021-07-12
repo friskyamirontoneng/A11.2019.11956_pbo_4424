463ecaff5082ecd036e84395d456996b30d60574
friskyamirontoneng <80090825+friskyamirontoneng@users.noreply.github.com>
2021-07-11 21:23:44 -0700

    Add files via upload

---
+++ UAS PBO/perpus/src/main/java/Model/mTable.java
@@ -0,0 +1,20 @@
+/*
+Nama: Friskya Mirontoneng
+Nim: A11.2019.11956
+Kelas: A11.4424
+ */
+package Model;
+
+/**
+ *
+ * @author Friskya 
+ */
+public class mTable {
+    public int id;
+    public String judul;
+    public String tanggal_pinjam;
+    public String tanggal_kembali;
+    public String tanggal_harus_kembali;
+    public int denda;
+    public int biaya_sewa;
+}
